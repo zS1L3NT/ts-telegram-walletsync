b6c34788a5f417756f8b202a4b007959a2931211
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-09-25 02:11:31 +0800

    ✨ trust date in client_reference over stated date

--- src/dbs.ts
+++ src/dbs.ts
@@ -1,22 +1,35 @@
 import { readFileSync, writeFileSync } from "fs"
 import { resolve } from "path"
 
-const dbs = readFileSync(resolve("raw/dbs.csv"), "utf-8")
+const dbs = readFileSync(resolve("raw/dbs-raw.csv"), "utf-8")
 
 const transactions = dbs
 	.split("\n")
 	.slice(21, -5)
 	.map(line => {
-		const [date, , , , debit, credit, client_reference, additional_reference] = line.split(",")
+		const [stated_date, , , , debit, credit, client_reference, additional_reference] =
+			line.split(",")
+		const real_date = client_reference?.match(/([0-9]{2}[A-Z]{3})/)?.[1]
+
+		const date = real_date
+			? new Date(`${real_date.slice(0, 2)} ${real_date.slice(2)} ${stated_date!.slice(-4)}`)
+			: new Date(stated_date!)
+
 		return [
-			new Date(date!).toLocaleDateString("en-SG", {
+			date.toLocaleDateString("en-SG", {
 				day: "2-digit",
 				month: "long",
 				year: "numeric",
 			}),
 			debit!.trim() ? -parseFloat(debit!) : parseFloat(credit!),
 			[client_reference, additional_reference].filter(Boolean).join(" | "),
-		].join(",")
+		]
 	})
 
-writeFileSync(resolve("raw/dbs.csv"), "Date,Amount,Description\n" + transactions.join("\n"))
+writeFileSync(
+	resolve("raw/dbs.csv"),
+	"Date,Amount,Description\n" +
+		transactions
+			.sort((a, b) => new Date(b[0]!).getTime() - new Date(a[0]!).getTime())
+			.join("\n"),
+)
